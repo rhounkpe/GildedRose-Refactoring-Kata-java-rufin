a0e22edce52d226b7a977520559b92c2bdf9da18
Hounkpe Metogbe Rufin <rhounkpe@gmail.com>
2024-03-18 08:33:19 +0100

    Inline and reformat code

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -49,24 +49,12 @@ class GildedRose {
 
     private void updateQuality(Item item) {
         if (item.name.equals("Aged Brie")) {
-            updateQualityForAgedBrieAndTafkal80EtcConcert(item);
-        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")){
-            updateQualityForAgedBrieAndTafkal80EtcConcert(item);
-        }
-        else {
-            if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.quality = item.quality - 1;
-                }
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
             }
-        }
-    }
-
-    private void updateQualityForAgedBrieAndTafkal80EtcConcert(Item item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-
-            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
                 if (item.sellIn < 11) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
@@ -79,6 +67,13 @@ class GildedRose {
                     }
                 }
             }
+        } else {
+            if (item.quality > 0) {
+                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    item.quality = item.quality - 1;
+                }
+            }
         }
     }
+
 }
