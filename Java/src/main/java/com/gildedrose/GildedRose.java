9ac39e9c3887267d5a09e7cc8ed708195265ee57
Hounkpe Metogbe Rufin <rhounkpe@gmail.com>
2024-03-18 08:37:38 +0100

    Improve readability

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -68,8 +68,8 @@ class GildedRose {
                 }
             }
         } else {
-            if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (item.quality > 0) {
                     item.quality = item.quality - 1;
                 }
             }
